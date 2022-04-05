42e64df82f9041aedc5922ef7a7d5e0630208aff
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2022-04-05 06:21:01 -0400

    Initial commit

---
+++ Utlilities Solution/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class MainFrm
+    Private Sub BillsBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles BillsBindingNavigatorSaveItem.Click
+        Try
+            Me.Validate()
+            Me.BillsBindingSource.EndEdit()
+            Me.TableAdapterManager.UpdateAll(Me.UtilitiesDataSet)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+
+    End Sub
+
+    Private Sub MainFrm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'UtilitiesDataSet.Bills' table. You can move, or remove it, as needed.
+        Try
+            Me.BillsTableAdapter.Fill(Me.UtilitiesDataSet.Bills)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+End Class
---
+++ Utlilities Solution/dbo.Bills.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Bills] (
+    [Month]       INT            IDENTITY (1, 1) NOT NULL,
+    [Electricity] DECIMAL (5, 2) NOT NULL,
+    [WATER]       DECIMAL (5, 2) NOT NULL,
+    [GAS]         DECIMAL (5, 2) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Month] ASC)
+);
+
